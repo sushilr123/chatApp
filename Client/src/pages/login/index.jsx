78301ead1da92530bb47c083a275dedeebd8c062
sushilr123 <sushilr@iitbhilai.ac.in>
2026-01-13 10:58:27 +0530

    create login page UI and add functionlity

--- Client/src/pages/login/index.jsx
+++ Client/src/pages/login/index.jsx
@@ -1,4 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
 function Login() {
-  return <div>Login Page</div>;
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+  const onformsubmit = (event) => {
+    event.preventDefault();
+    console.log(user);
+  };
+  return (
+    <div className="container">
+      <div className="container-back-img"></div>
+      <div className="container-back-color"></div>
+      <div className="card">
+        <div className="card_title">
+          <h1>Login here</h1>
+        </div>
+        <form action="" onSubmit={onformsubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+          <div className="button">
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
+      <div className="card_terms">
+        <span>Don't have an account? Signup</span>
+        <Link to="/signup">Signup Here</Link>
+      </div>
+    </div>
+  );
 }
 export default Login;
--- Client/src/pages/signup/index.jsx
+++ Client/src/pages/signup/index.jsx
@@ -1,23 +1,54 @@
-// import react from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 function Signup() {
+  const [user, setUser] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const onformsubmit = (event) => {
+    event.preventDefault();
+    console.log(user);
+  };
   return (
     <div className="container">
       <div className="container-back-img"></div>
       <div className="container-back-color"></div>
       <div className="card">
         <div className="card_title">
-          <h1>
-            Created Account Successfully
-          </h1>
+          <h1>Created Account Successfully</h1>
         </div>
-        <div className="form">
+        <div className="form" onSubmit={onformsubmit}>
           <form action="">
             <div className="column">
-              <input type="text" placeholder="First Name" />
-              <input type="text" placeholder="Last Name" />
+              <input
+                type="text"
+                placeholder="First Name"
+                value={user.firstName}
+                onChange={(e) =>
+                  setUser({ ...user, firstName: e.target.value })
+                }
+              />
+              <input
+                type="text"
+                placeholder="Last Name"
+                value={user.lastName}
+                onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+              />
             </div>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input
+              type="email"
+              placeholder="Email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+            />
             <div className="button">
               <button type="submit">Signup</button>
             </div>
@@ -25,10 +56,10 @@ function Signup() {
         </div>
         <div className="card_terms">
           <span>Already have an account? Login</span>
-          <a href="">Login Here</a>
+          <Link to="/login">Login Here</Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
 export default Signup;
